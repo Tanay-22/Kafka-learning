4a8ba78c5f56134e7da9c0823f6d302b07ab5b80
Tanay Pandey <tanay22pandey@gmail.com>
2024-12-18 20:20:54 +0530

    made the first producer with java

---
+++ Kafka-basics/src/main/java/com/tanay/kafka/demo/ProducerDemo.java
@@ -0,0 +1,39 @@
+package com.tanay.kafka.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo
+{
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args)
+    {
+        log.info("Hello");
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create Producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello Kafka !");
+
+        // send the data asynchronous
+        producer.send(producerRecord);
+
+        // flush and close the producer
+        producer.flush();
+        producer.close();
+    }
+}
